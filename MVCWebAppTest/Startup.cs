4cdbdd546900f7a1896c8164de4d0d2ec87953cd
Thomas <thmsphung2000@gmail.com>
2020-10-28 15:33:07 -0700

    Made it so that one view is associated with one model

--- MVCWebAppTest/Startup.cs
+++ MVCWebAppTest/Startup.cs
@@ -34,6 +34,9 @@ namespace MVCWebAppTest
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            // reading setting values from AppSettings.cs
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
