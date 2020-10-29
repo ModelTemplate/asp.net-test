dfed5a78d7b2f724b00f98322728717664bc6eb9
Thomas <thmsphung2000@gmail.com>
2020-10-29 11:38:38 -0700

    Unit test project added

---
+++ MVCWebAppTest/MVCWebAppTest/ModelUnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MVCWebAppTest
+{
+    public class ModelUnitTest1
+    {
+        [Fact]
+        public void ItemNoParam()
+        {
+
+        }
+    }
+}
